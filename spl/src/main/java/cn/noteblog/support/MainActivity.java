31aa38f242f86d4361ae0b9c4cf3b336ba593f99
FynnHan <809269260@qq.com>
2016-12-12 16:20:00 +0800

    首次提交

---
+++ spl/src/main/java/cn/noteblog/support/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.noteblog.support;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* 测试代码区 */
+    }
+}
