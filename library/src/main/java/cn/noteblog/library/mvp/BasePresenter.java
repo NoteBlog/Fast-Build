dad69db37f8af4257983d45116df8027beda170d
FynnHan <809269260@qq.com>
2016-12-27 17:04:01 +0800

    Jcenter依赖库版本更新为1.1.0，删除sources中没有使用的类和方法。

--- library/src/main/java/cn/noteblog/library/mvp/BasePresenter.java
+++ library/src/main/java/cn/noteblog/library/mvp/BasePresenter.java
@@ -8,9 +8,9 @@ import android.view.ViewGroup;
  * 这是一个对视图初始化及处理的接口
  * Created by Design on 2016/4/8.
  */
-public interface BasePresenter {
-
-    void init(LayoutInflater inflater, ViewGroup container);
-
-    View getView();
-}
+//public interface BasePresenter {
+//
+//    void init(LayoutInflater inflater, ViewGroup container);
+//
+//    View getView();
+//}
